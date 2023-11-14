963a4ed81021efad395a9eaf7aca8180b96950ce
Brandon Roffis <brandon.roffis@ferguson.com>
2023-11-14 15:13:25 -0500

    get comments

--- .github/actions/issue-comment-check/scripts/issue-comment-check.js
+++ .github/actions/issue-comment-check/scripts/issue-comment-check.js
@@ -1,11 +1,25 @@
-module.exports = ({ context }) => {
+module.exports = async ({ context, github }) => {
   const {
     payload: { comment, issue, repository },
   } = context;
 
-  console.log({ comment, issue, repository });
+  //   console.log({ comment, issue, repository });
 
   // comment.body for issue text
   // issue.number for issue_number
-  //
+  // repository.name for repo
+  // repository.owner.login for owner
+
+  const {
+    name,
+    owner: { login },
+  } = context.payload.repository;
+
+  const comments = await github.rest.issues.listComments({
+    owner: login,
+    repo: name,
+    issue_number: issue.number,
+  });
+
+  console.log([comments]);
 };
